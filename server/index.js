54ab55b3eaf78f9bdd1397bacc0ff46033befee2
Tanya <tanya.nijhawan2021@vitstudent.ac.in>
2023-07-08 00:01:54 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-//external packages
+// external packages
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
@@ -7,29 +7,28 @@ import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
-//pull our variables from .env files
+// pull our variables from .env files
 dotenv.config();
 
-const app=express();
+const app = express();
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 
-//middleware to handle routes
-app.use('/api/v1/posts',postRoutes);
-app.use('/api/v1/dalle',dalleRoutes);
+// middleware to handle routes
+app.use('/api/v1/post', postRoutes); // Update the route URL to '/api/v1/post'
+app.use('/api/v1/dalle', dalleRoutes);
 
-app.get('/',async(req,res)=>{
-    res.send('Hello from DALL-E 2.0');
-})
+app.get('/', async (req, res) => {
+  res.send('Hello from DALL-E 2.0');
+});
 
-const server=async()=>{
-    try{
-        connectDB(process.env.MONGODB_URL)
-            app.listen(3000,(console.log('Server is running on port 3000')))
-    }catch(error){
-        console.log(error);
-    }
-    
-}
+const server = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL); // Make sure to wait for the DB connection before starting the server
+    app.listen(3000, () => console.log('Server is running on port 3000'));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 server();
